b4775978ce3fca6344d2de0ec9e94034504bb6a7
Christopher Gunn <christopher.gunn@ericsson.com>
2024-03-22 10:20:29 +0000

    The age of retirement went up

--- CoGrammarTasks/Task 3/age-quiz.py
+++ CoGrammarTasks/Task 3/age-quiz.py
@@ -4,9 +4,9 @@ age = int(input("How old are you? \n"))
 # Check if the user is over 100 years
 if age > 100:
     print("Sorry, you're dead.")
-# Check if the user is 65 or over
-elif age >= 65:
-    print("Enjoy your retirement!")
+# Check if the user is 71 or over
+elif age >= 71:
+    print("You've finally stopped working, thank god!")
 # Check if the user is 40 or over
 elif age >= 40:
     print("You're over the hill.")
